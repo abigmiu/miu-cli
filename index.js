3754049c02fb63398668311d890ef1c24377d96e
tangying <tangying@ganguo.hk>
2022-06-28 16:03:42 +0800

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const fs = require('fs')
+
+//获得命令运行时的路径
+const getCwd = () => process.cwd();
+
+const { program } = require('commander');
+const handleCreate = () => {
+    fs.writeFileSync(`${getCwd()}/.editorConfig`, fs.readFileSync(`${__dirname}/template/.editorConfig`))
+    fs.writeFileSync(`${getCwd()}/.prettierrc.js`, fs.readFileSync(`${__dirname}/template/.prettierrc.js`))
+    console.log('🎉 生成成功')
+}
+
+program
+    .command('create')
+    .description('生成editorConfig和prettier配置文件')
+    .action(() => {
+        handleCreate()
+    })
+
+program.parse(process.argv);
